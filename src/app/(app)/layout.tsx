5b2ef79b23ca05bc72ffe0bfb1ae2f5827527f0f
Malek133 <amazighmalek@gmail.com>
2026-01-09 13:01:01 +0100

    Dashboard Layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,8 @@
+import {getUserDal} from '@/app/dal/user-dal'
 import withAuth from '@/components/auth/withAuth'
+import Sidebar from '@/components/dashboard/sidebar'
+
+
 import {Metadata} from 'next'
 import {PropsWithChildren} from 'react'
 
@@ -8,9 +12,12 @@ export const metadata: Metadata = {
 }
 
 const DashboardLayout = async ({children}: PropsWithChildren) => {
+  const user = await getUserDal()
   return (
     <div className="flex h-screen flex-col md:flex-row xl:justify-center">
-      <div className="w-full p-4 md:w-64"></div>
+      <div className="w-full p-4 md:w-64">
+        <Sidebar user={user} />
+      </div>
       <div className="w-full max-w-7xl p-4 pt-8">{children}</div>
     </div>
   )
